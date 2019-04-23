84d22d3e229db2783810d3e5a13147a76304d1e5
Vladyslav <oleshko.vladyslav@gmail.com>
2019-04-23 20:39:06 +0300

    Phase II

--- game.js
+++ game.js
@@ -1,52 +1,75 @@
 let gameContainer;
 let gameContext;
-let ballConfig;
-let platformConfig;
+let ball;
 let engineConfig;
+let leftPlatform;
+let rightPlatform;
+let gameField;
+
+class Platform {
+    constructor(positionX, positionY, width, height) {
+        this.posX = positionX;
+        this.posY = positionY;
+        this.width = width;
+        this.height = height;
+    }
+}
+
+class Ball {
+    constructor(positionX, positionY, radius, angle, speedX, speedY) {
+        this.posX = positionX;
+        this.posY = positionY;
+        this.radius = radius;
+        this.angle = angle;
+        this.speedX = speedX;
+        this.speedY = speedY;
+    }
+}
 
 const initDefaultValues = () => {
-    platformConfig = {
-        width: 10,
-        height: 100,
-        positionX: 20,
-        positionY: (gameContainer.height - this.height) / 2
-    }
-    ballConfig = {
-        positionX: 50,
-        positionY: 50,
-        width: 10,
-        height: 10
-    }
     engineConfig = {
         fps: 30
     }
+
+    ball = new Ball(gameContainer.width / 2, gameContainer.height / 2, 5, 0, 10, 5);
+    gameField = new Platform(0, 0, gameContainer.width, gameContainer.height);
+    leftPlatform = new Platform(10,(gameContainer.height - 70) / 2, 5, 70);
+    rightPlatform = new Platform(gameContainer.width - 10, (gameContainer.height - 70) / 2, 5, 70);
 }
 
 const calculateGame = () => {
-    ballConfig.positionX += 5;
-    ballConfig.positionY += 5;
+    ball.posX += ball.speedX;
+    ball.posY += ball.speedY;
+
+    if (ball.posX > (gameContainer.width - ball.radius) || (ball.posX - ball.radius) < 0) {
+        ball.speedX *= -1;
+    }
+
+    if (ball.posY > (gameContainer.height - ball.radius) || (ball.posY - ball.radius) < 0) {
+        ball.speedY *= -1;
+    }
+}
+
+const drawRect = (color, {posX, posY, width, height}) => {
+    gameContext.fillStyle = color;
+    gameContext.fillRect(posX, posY, width, height);
+}
+
+const drawCircle = (color, {posX, posY, radius, angle}) => {
+    gameContext.fillStyle = color;
+    gameContext.beginPath();
+    gameContext.arc(posX, posY, radius, angle, Math.PI * 2, true);
+    gameContext.fill();
 }
 
 const drawGame = () => {
-    // Platform
-    gameContext.fillStyle = 'white';
-    gameContext.fillRect(platformConfig.positionX, platformConfig.positionY, platformConfig.width, platformConfig.height);
-    // Ball
-    gameContext.fillStyle = 'yellow';
-    gameContext.fillRect(ballConfig.positionX, ballConfig.positionY, ballConfig.width, ballConfig.height);
-}
-
-const drawGameField = () => {
-    gameContext.fillStyle = 'black';
-    gameContext.fillRect(0, 0, gameContainer.width, gameContainer.height);
-}
-
-const clearGameField = () => {
-    gameContext.clearRect(0, 0, gameContainer.width, gameContainer.height);
+    drawRect('yellow', leftPlatform); // Left platform
+    drawRect('yellow', rightPlatform); // Right platform
+    drawCircle('yellow', ball); // Ball
 }
 
 const gameEngine = () => {
-    drawGameField();
+    drawRect('black', gameField); // Initial field
     calculateGame();
     drawGame();
 }
