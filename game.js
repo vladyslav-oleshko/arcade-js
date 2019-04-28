39233aea5b6e0c82b4b6c89bb2d5f82490de3cf3
Vladyslav <oleshko.vladyslav@gmail.com>
2019-04-28 17:12:42 +0300

    Pre-alpha release

--- game.js
+++ game.js
@@ -2,16 +2,18 @@ let gameContainer;
 let gameContext;
 let ball;
 let engineConfig;
-let leftPlatform;
-let rightPlatform;
+let userPlatform;
+let AIPlatform;
 let gameField;
 
 class Platform {
-    constructor(positionX, positionY, width, height) {
+    constructor(positionX, positionY, width, height, AISpeed, score) {
         this.posX = positionX;
         this.posY = positionY;
         this.width = width;
         this.height = height;
+        this.AISpeed = AISpeed;
+        this.score = score;
     }
 }
 
@@ -28,21 +30,54 @@ class Ball {
 
 const initDefaultValues = () => {
     engineConfig = {
-        fps: 30
+        fps: 30,
+        AIPlatformPadding: 35
     }
 
-    ball = new Ball(gameContainer.width / 2, gameContainer.height / 2, 5, 0, 10, 5);
+    ball = new Ball(gameContainer.width / 2, gameContainer.height / 2, 5, 0, 15, 5);
     gameField = new Platform(0, 0, gameContainer.width, gameContainer.height);
-    leftPlatform = new Platform(10,(gameContainer.height - 70) / 2, 5, 70);
-    rightPlatform = new Platform(gameContainer.width - 10, (gameContainer.height - 70) / 2, 5, 70);
+    userPlatform = new Platform(0,(gameContainer.height - 70) / 2, 5, 70, 0, 0);
+    AIPlatform = new Platform(gameContainer.width - 5, (gameContainer.height - 70) / 2, 5, 100, 5, 0);
+}
+
+const resetBall = () => {
+    ball.posX = gameContainer.width / 2;
+    ball.posY = gameContainer.height / 2;
+    ball.speedX *= -1;
+}
+
+const AIMovement = () => {
+    const platformCenter = AIPlatform.posY + (AIPlatform.height / 2);
+
+    if (platformCenter < (ball.posY - engineConfig.AIPlatformPadding)) {
+        AIPlatform.posY += AIPlatform.AISpeed;
+    } else if (platformCenter > (ball.posY + engineConfig.AIPlatformPadding)) {
+        AIPlatform.posY -= AIPlatform.AISpeed;
+    }
 }
 
 const calculateGame = () => {
+    AIMovement();
+
     ball.posX += ball.speedX;
     ball.posY += ball.speedY;
 
-    if (ball.posX > (gameContainer.width - ball.radius) || (ball.posX - ball.radius) < 0) {
-        ball.speedX *= -1;
+    if (ball.posX > gameContainer.width) {
+        if (ball.posY > AIPlatform.posY && ball.posY < (AIPlatform.posY + AIPlatform.height)) {
+            ball.speedX *= -1;
+        } else {
+            userPlatform.score += 1;
+            resetBall();
+        }
+    }
+
+    if (ball.posX < 0) {
+        if (ball.posY > userPlatform.posY && ball.posY < (userPlatform.posY + userPlatform.height)) {
+            ball.speedX *= -1;
+        } else {
+            AIPlatform.score += 1;
+            resetBall();
+        }
     }
 
     if (ball.posY > (gameContainer.height - ball.radius) || (ball.posY - ball.radius) < 0) {
@@ -62,10 +97,16 @@ const drawCircle = (color, {posX, posY, radius, angle}) => {
     gameContext.fill();
 }
 
+const drawScore = () => {
+    gameContext.fillText(userPlatform.score, 100, 100);
+    gameContext.fillText(AIPlatform.score, gameContainer.width - 100, 100);
+}
+
 const drawGame = () => {
-    drawRect('yellow', leftPlatform); // Left platform
-    drawRect('yellow', rightPlatform); // Right platform
+    drawRect('yellow', userPlatform); // User platform
+    drawRect('yellow', AIPlatform); // AI platform
     drawCircle('yellow', ball); // Ball
+    drawScore(); // Score
 }
 
 const gameEngine = () => {
@@ -74,10 +115,21 @@ const gameEngine = () => {
     drawGame();
 }
 
+const calculateMousePosition = (event) => {
+    const rect = gameContainer.getBoundingClientRect();
+    const root = document.documentElement;
+    return {
+        x: event.clientX - rect.left - root.scrollLeft,
+        y: event.clientY - rect.top - root.scrollTop
+    }
+}
+
 window.onload = () => {
     gameContainer = document.getElementById('gameContainer');
     gameContext = gameContainer.getContext('2d');
     initDefaultValues();
-    // Run animation
     setInterval(gameEngine, 1000 / engineConfig.fps);
+    gameContainer.addEventListener('mousemove', (event) => {
+        userPlatform.posY = calculateMousePosition(event).y - userPlatform.height / 2;
+    })
 }
